b360af3bd551b7250c40578697e12798330f327f
Артем <artyom27000@mail.ru>
2022-10-18 23:36:07 +0300

    переделал синтезатор голоса почти доделал json

--- main.py
+++ main.py
@@ -7,28 +7,26 @@ from fuzzywuzzy import fuzz
 from win32com.client import Dispatch
 
 import stt
-from module import cfg, tts
+from module.tts import TTSpeaker
 
 path = "C:\\Program Files\\Adobe\\Adobe Photoshop 2020\\Photoshop.exe"
+stocks = {}
 
 
 def read_json():
-    """
-    Метод читает файл json
-    :return: возвращает путь объекта
-    """
     with open('config_.json', 'r', encoding='utf-8') as file:
-        stock = json.load(file)
-        print(json.dumps(stock,
-                         sort_keys=False,
-                         indent=4,
-                         ensure_ascii=False,
-                         separators=(',', ': ')))
-
-    # Find_Photoshop("C:\Program Files")
+        return json.load(file)
 
 
-def subprocess_setup(process: str):
+def subprocess_setup(process: str) :
+    """
+    Метод производит запуск приложения в отдельном потоке
+    @return Результат выполнения
+    ---------------------------------------------------------
+                 ВНИМАНЕ МЕТОД НЕ ДОПИСАН И НЕ РАБОТАЕТ (ИДУТ СТРОИТЕЛЬНЫЕ РАБОТЫ)
+                            НЕ ПЫТАЙТЕСЬ ЕГО ВЫЗВАТЬ ИЛИ УДАЛИТЬ
+    ---------------------------------------------------------------
+    """
     try:
         app = Dispatch(process)
         return app
@@ -38,120 +36,134 @@ def subprocess_setup(process: str):
 
 def va_respond(voice: str):
     print(voice)
-    if voice.startswith(cfg.Pak_allias):
+    if voice.startswith(tuple(stocks["other"]["pack_atlas"])):
         # обращаются к ассистенту
         cmd = recognize_cmd(filter_cmd(voice))
 
-        if cmd['cmd'] not in cfg.Pak_commandlist.keys():
-            tts.va_speak("")
+        if cmd['cmd'] not in stocks["pack_command-list"].keys():
+            model.load_bath("")
         else:
             execute_cmd(cmd['cmd'])
 
 
 def filter_cmd(raw_voice: str):
     cmd = raw_voice
+    for x in stocks["other"]["pack_atlas"]:
+        cmd = cmd.replace(x, "").strip()  # форматирует строку
 
-    for x in cfg.Pak_allias:
+    for x in stocks["other"]["var_tbr"]:  # cfg.VAP_TBR:
         cmd = cmd.replace(x, "").strip()
-
-    for x in cfg.VAP_TBR:
-        cmd = cmd.replace(x, "").strip()
-    print(cmd, 'cmd')
     return cmd
 
 
 def recognize_cmd(cmd: str):
-    rc = {'cmd': '', 'percent': 0}
-    for c, v in cfg.Pak_commandlist.items():
-
+    """
+    на вход подается
+    распознает команду(к примеру, если есть отличие на пару букв или если это другая форма одного слова)
+    """
+    DICTIONARY_SIM = {'cmd': '', 'percent': 0}
+    for CMD, v in stocks["pack_command-list"].items():
         for x in v:
-            vrt = fuzz.ratio(cmd, x)
-            if vrt > rc['percent']:
-                rc['cmd'] = c
-                rc['percent'] = vrt
+            SIMILARITY = fuzz.ratio(cmd, x)  # сравнивается команда с командой в файле
+            if SIMILARITY > DICTIONARY_SIM['percent']:  # если сравнение больше 0 то
+                # дальше в словарь записываются данные: КАКАЯ КОМАНДА И НА СКОЛЬКО ПОХОЖА
+                DICTIONARY_SIM['cmd'] = CMD
+                DICTIONARY_SIM['percent'] = SIMILARITY
 
-    return rc
+    return DICTIONARY_SIM
 
 
 def execute_cmd(cmd: str):
     if cmd == 'help':
-        text = "Я умею: ..."
-        text += "открывать фотошоп ..."
-        text += "создавать холсты ..."
-        text += "и слои"
-        text += "а также отменять действия"
-        threading.Thread(target=tts.va_speak(text))
+        text = "Йа умею: ..." + \
+               "аткрыв+ать фотош+оп ..." + \
+               "саздав+ать холст+ы ..." + "и  сла и ..." + \
+               "а т++акже отменйать действийа"
 
-    elif cmd == 'openPS':
-        speak = 'Открыла'
+        threading.Thread(target=model.load_bath(text))
+        threading.Thread(target=(model.va_speak()))
 
+    elif cmd == 'open_ps':
+        speak = 'аткрыл'
         threading.Thread(target=(os.startfile(path)))
-
-        tts.va_speak(speak)
+        model.load_bath(speak)
+        threading.Thread(target=(model.va_speak()))
 
     elif cmd == 'holst':
         keyboard.send('ctrl+n')
-    elif cmd == 'New_Layer':
-        app = subprocess_setup("Photoshop.Application")
 
-        docRef = app.Application.ActiveDocument
-        layerRef = docRef.ArtLayers.Add()
+    elif cmd == 'new_layer':
+        speak = 'саздал'
+        model.load_bath(speak)
+        threading.Thread(target=(model.va_speak()))
+        subprocess_setup("Photoshop.Application")
 
     elif cmd == 'CTRLC':
         threading.Thread(target=keyboard.send('ctrl+c'))
 
-    elif cmd == 'TextLayer':
-        app = subprocess_setup("Photoshop.Application")
+    elif cmd == 'text_layer':
+        try:
+            app = subprocess_setup("Photoshop.Application")
+            # pywintypes.com_error: (-2147417846, 'Фильтр сообщений выдал диагностику о занятости приложения.', None, None)
+            app.Preferences.TypeUnits = 5
+            app.displayDialogs = 3
+            newTextLayer = app.Application.ActiveDocument.ArtLayers.Add()
+            newTextLayer.Kind = 2
+            newTextLayer.TextItem.Contents = "Your Text!"
+            newTextLayer.TextItem.Contents = "Hello, World!"
+        except Exception as e:
+            print(e, "ПРОВЕРЬ ДИРЕКТОРИЮ ФОТОШОПА")
 
-        app.Preferences.RulerUnits = 2
-        app.Preferences.TypeUnits = 5
-
-        app.displayDialogs = 3
-        newTextLayer = app.Application.ActiveDocument.ArtLayers.Add()
-        newTextLayer.Kind = 2
-        newTextLayer.TextItem.Contents = "Your Text!"
-        newTextLayer.TextItem.Contents = "Hello, World!"
     elif cmd == 'ctrlz':
-        keyboard.send('ctrl+alt+z')
-    elif cmd == 'Return':
+        try:
+            keyboard.send('ctrl+alt+z')
+        except Exception as e:
+            print(e)
+    elif cmd == 'return':
         keyboard.send('ctrl+shift+z')
+
     elif cmd == 'CTRLV':
         keyboard.send('ctrl+v')
+
     elif cmd == 'CUT':
         keyboard.send('f2')
+
     elif cmd == 'open_file':
         keyboard.send('ctrl+o')
+
     elif cmd == 'increase':
         keyboard.send('ctrl,+')
+
     elif cmd == 'decrease':
         keyboard.send('ctrl+-')
+
     elif cmd == 'display':
         keyboard.send('ctrl+alt+0')
+
     elif cmd == 'scale':
         keyboard.send('z')
+
     elif cmd == 'layers_panel':
         keyboard.send('f7')
+
     elif cmd == 'copy_layer':
         keyboard.send('ctrl+j')
+
     elif cmd == 'group_layer':
         keyboard.send('ctrl+g')
+
     elif cmd == 'disgroup':
         keyboard.send('ctrl+shift+g')
+
     elif cmd == 'select_layer':
         keyboard.send('ctrl+alt+a')
-    elif cmd == 'scale':
-        keyboard.send('z')
-    elif cmd == 'scale':
-        keyboard.send('z')
-    elif cmd == 'scale':
-        keyboard.send('z')
+
     elif cmd == 'scale':
         keyboard.send('z')
 
 
 if __name__ == "__main__":
-    read_json()
-    # print(*passage('photoshop.exe', 'C:\Program Files'))
-    print(f'Голосовой помощник{cfg.VAP_name} версии {cfg.VAP_ver} готов нести службу\n',
-          "чтобы выйти нажмите Ctrl+Q")
-    threading.Thread(target=stt.va_listen(va_respond))
+    stocks = read_json()
+    model = TTSpeaker()
+    model.load_bath(stt.va_listen(va_respond))
+    model.va_speak()
--- module/tts.py
+++ module/tts.py
@@ -2,14 +2,15 @@ import torch
 import sounddevice as sd
 import time
 
+from module import cfg
+
 language = 'ru'
 model_id = 'ru_v3'
-sample_rate = 48000 # 48000
-speaker = 'kseniya' # aidar, baya, kseniya, xenia, random
+sample_rate = 48000  # 48000
+speaker = 'aidar'  # aidar, baya, kseniya, xenia, random
 put_accent = True
 put_yo = True
-device = torch.device('cpu') # cpu или gpu
-
+device = torch.device('cpu')  # cpu или gpu
 
 model, _ = torch.hub.load(repo_or_dir='snakers4/silero-models',
                           model='silero_tts',
@@ -18,14 +19,29 @@ model, _ = torch.hub.load(repo_or_dir='snakers4/silero-models',
 model.to(device)
 
 
-# воспроизводим
-def va_speak(what: str):
-    audio = model.apply_tts(text=what+"..",
-                            speaker=speaker,
-                            sample_rate=sample_rate,
-                            put_accent=put_accent,
-                            put_yo=put_yo)
+class TTSpeaker:
+    def __init__(self):
+        self._example_batch = 'привет ... меня  завут  п+око ... Йа тв+ой галсов+ой пам+ошник'
+        print(f'Голосовой помощник{cfg.VAP_name} версии {cfg.VAP_ver} готов нести службу\n')
 
-    sd.play(audio, sample_rate * 1.05)
-    time.sleep((len(audio) / sample_rate) + 0.5)
-    sd.stop()
+    def load_bath(self, batch):
+        self._example_batch = batch
+        # for let in batch:
+        #     if let in "1234567890":
+        #         self._example_batch = "цифры исключенны"
+        #         break
+
+    def va_speak(self):
+        audio = model.apply_tts(text=self._example_batch+"...",
+                                speaker=speaker,
+                                sample_rate=sample_rate,
+                                put_accent=put_accent,
+                                put_yo=put_yo)
+
+        sd.play(audio, sample_rate * 1.05)
+        time.sleep((len(audio) / sample_rate) + 0.5)
+        sd.stop()
+
+if __name__ == "__main__":
+    poko = TTSpeaker()
+    poko.va_speak()
