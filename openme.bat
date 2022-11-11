cf2c98b1b05b7f427cd9387dcdb55363d1c0c4d5
нефот <_@email.com>
2022-11-11 21:03:35 +0300

    батник переделал

--- openme.bat
+++ openme.bat
@@ -10,10 +10,10 @@ pip install python-Levenshtein
 pip install vosk 
 pip install torch==1.12.1 
 pip install --upgrade omegaconf 
-pip install numpy"
+pip install numpy==1.23.4
 pip install -U pyinstaller
 pip install PyQt5
 pip install tk
 
 python -m venv venv
-cmd /k "cd /d F:\Desktop\Enifot\AudioBot\venv\Scripts & activate & pip install PyQt5 & pip install sounddevice & python.exe -m pip install --upgrade pip & pip install keyboard & pip install fuzzywuzzy & pip install pywin32 & python -m pip install --upgrade pywin32 & pip install python-Levenshtein & pip install vosk & pip install torch==1.12.1 & pip install --upgrade omegaconf & pip install numpy & pip install tk exit"
+cmd /k "cd /d F:\Desktop\Enifot\AudioBot\venv\Scripts & activate & pip install PyQt5 & pip install sounddevice & python.exe -m pip install --upgrade pip & pip install keyboard & pip install fuzzywuzzy & pip install pywin32 & python -m pip install --upgrade pywin32 & pip install python-Levenshtein & pip install vosk & pip install torch==1.12.1 & pip install --upgrade omegaconf & pip install numpy==1.23.4 & pip install tk exit"
